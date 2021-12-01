38579ebea0b3139057e0f2e6cb67bb7a21ba8969
Nguyen Dinh Nguyen <83230901+DinhNguyenNguyen@users.noreply.github.com>
2021-12-01 12:10:12 +0700

    login

--- PM_QuanLyThuVien/QuanLyThuVien/QuanLyThuVien/From/frmLogin.Designer.cs
+++ PM_QuanLyThuVien/QuanLyThuVien/QuanLyThuVien/From/frmLogin.Designer.cs
@@ -28,131 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             DevExpress.XtraEditors.Controls.EditorButtonImageOptions editorButtonImageOptions1 = new DevExpress.XtraEditors.Controls.EditorButtonImageOptions();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject1 = new DevExpress.Utils.SerializableAppearanceObject();
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject2 = new DevExpress.Utils.SerializableAppearanceObject();
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject3 = new DevExpress.Utils.SerializableAppearanceObject();
             DevExpress.Utils.SerializableAppearanceObject serializableAppearanceObject4 = new DevExpress.Utils.SerializableAppearanceObject();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
-            this.btnDangNhap = new DevExpress.XtraEditors.SimpleButton();
-            this.btnThoat = new DevExpress.XtraEditors.SimpleButton();
-            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.imageEdit1 = new DevExpress.XtraEditors.PictureEdit();
-            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtPass = new DevExpress.XtraEditors.ButtonEdit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
+            this.btnThoat = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDangNhap = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
+            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
             this.SuspendLayout();
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label2.Location = new System.Drawing.Point(112, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(282, 36);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "QUẢN LÝ THƯ VIỆN";
-            // 
-            // btnXoa
-            // 
-            this.btnXoa.Appearance.BorderColor = System.Drawing.Color.Maroon;
-            this.btnXoa.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnXoa.Appearance.Options.UseBorderColor = true;
-            this.btnXoa.Appearance.Options.UseFont = true;
-            this.btnXoa.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnXoa.Location = new System.Drawing.Point(104, 166);
-            this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(94, 34);
-            this.btnXoa.TabIndex = 4;
-            this.btnXoa.Text = "Xoá";
-            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
-            // 
-            // btnDangNhap
-            // 
-            this.btnDangNhap.Appearance.BorderColor = System.Drawing.Color.Maroon;
-            this.btnDangNhap.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDangNhap.Appearance.Options.UseBorderColor = true;
-            this.btnDangNhap.Appearance.Options.UseFont = true;
-            this.btnDangNhap.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnDangNhap.Location = new System.Drawing.Point(223, 166);
-            this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(94, 34);
-            this.btnDangNhap.TabIndex = 4;
-            this.btnDangNhap.Text = "Đăng Nhập";
-            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
-            this.btnDangNhap.MouseHover += new System.EventHandler(this.btnDangNhap_MouseHover);
-            // 
-            // btnThoat
-            // 
-            this.btnThoat.Appearance.BorderColor = System.Drawing.Color.Maroon;
-            this.btnThoat.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThoat.Appearance.Options.UseBorderColor = true;
-            this.btnThoat.Appearance.Options.UseFont = true;
-            this.btnThoat.AppearanceHovered.BackColor = System.Drawing.Color.White;
-            this.btnThoat.AppearanceHovered.Options.UseBackColor = true;
-            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnThoat.Location = new System.Drawing.Point(343, 166);
-            this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(94, 34);
-            this.btnThoat.TabIndex = 4;
-            this.btnThoat.Text = "Thoát";
-            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
-            // 
-            // labelControl1
-            // 
-            this.labelControl1.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl1.Appearance.Options.UseFont = true;
-            this.labelControl1.Location = new System.Drawing.Point(132, 117);
-            this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(66, 20);
-            this.labelControl1.TabIndex = 5;
-            this.labelControl1.Text = "Mật khẩu";
-            // 
-            // labelControl2
-            // 
-            this.labelControl2.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelControl2.Appearance.Options.UseFont = true;
-            this.labelControl2.Location = new System.Drawing.Point(129, 83);
-            this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(69, 20);
-            this.labelControl2.TabIndex = 5;
-            this.labelControl2.Text = "Tài khoản";
-            // 
-            // imageEdit1
-            // 
-            this.imageEdit1.EditValue = ((object)(resources.GetObject("imageEdit1.EditValue")));
-            this.imageEdit1.Location = new System.Drawing.Point(12, 57);
-            this.imageEdit1.Name = "imageEdit1";
-            this.imageEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
-            this.imageEdit1.Properties.Appearance.Options.UseBackColor = true;
-            this.imageEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.imageEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.imageEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Squeeze;
-            this.imageEdit1.Size = new System.Drawing.Size(111, 117);
-            this.imageEdit1.TabIndex = 6;
-            this.imageEdit1.TabStop = true;
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.EditValue = "admin";
-            this.txtUsername.Location = new System.Drawing.Point(222, 80);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Properties.Appearance.Options.UseFont = true;
-            this.txtUsername.Size = new System.Drawing.Size(215, 26);
-            this.txtUsername.TabIndex = 2;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::QuanLyThuVien.Properties.Resources.login;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // txtPass
             // 
             this.txtPass.EditValue = "admin";
-            this.txtPass.Location = new System.Drawing.Point(222, 116);
+            this.txtPass.Location = new System.Drawing.Point(101, 123);
             this.txtPass.Margin = new System.Windows.Forms.Padding(2);
             this.txtPass.Name = "txtPass";
             this.txtPass.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -163,9 +70,60 @@
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Glyph, "", -1, true, true, false, editorButtonImageOptions1, new DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, serializableAppearanceObject2, serializableAppearanceObject3, serializableAppearanceObject4, "", null, null, DevExpress.Utils.ToolTipAnchor.Default)});
             this.txtPass.Properties.UseSystemPasswordChar = true;
             this.txtPass.Properties.Click += new System.EventHandler(this.txtReNewPass_Properties_Click);
-            this.txtPass.Size = new System.Drawing.Size(216, 26);
-            this.txtPass.TabIndex = 19;
-            this.txtPass.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.txtPass_ButtonClick);
+            this.txtPass.Size = new System.Drawing.Size(269, 26);
+            this.txtPass.TabIndex = 24;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Appearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnThoat.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Appearance.Options.UseBorderColor = true;
+            this.btnThoat.Appearance.Options.UseFont = true;
+            this.btnThoat.AppearanceHovered.BackColor = System.Drawing.Color.White;
+            this.btnThoat.AppearanceHovered.Options.UseBackColor = true;
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.Location = new System.Drawing.Point(276, 164);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(94, 34);
+            this.btnThoat.TabIndex = 21;
+            this.btnThoat.Text = "Thoát";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Appearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnDangNhap.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Appearance.Options.UseBorderColor = true;
+            this.btnDangNhap.Appearance.Options.UseFont = true;
+            this.btnDangNhap.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDangNhap.Location = new System.Drawing.Point(163, 164);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(94, 34);
+            this.btnDangNhap.TabIndex = 22;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Appearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnXoa.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Appearance.Options.UseBorderColor = true;
+            this.btnXoa.Appearance.Options.UseFont = true;
+            this.btnXoa.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnXoa.Location = new System.Drawing.Point(52, 164);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(94, 34);
+            this.btnXoa.TabIndex = 23;
+            this.btnXoa.Text = "Xoá";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.EditValue = "admin";
+            this.txtUsername.Location = new System.Drawing.Point(101, 90);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Properties.Appearance.Options.UseFont = true;
+            this.txtUsername.Size = new System.Drawing.Size(269, 26);
+            this.txtUsername.TabIndex = 20;
             // 
             // frmLogin
             // 
@@ -175,16 +133,13 @@
             this.Appearance.Options.UseFont = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 232);
+            this.ClientSize = new System.Drawing.Size(435, 232);
             this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.labelControl2);
-            this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.btnXoa);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.imageEdit1);
+            this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Tahoma", 10F);
             this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -198,23 +153,20 @@
             this.Text = "Đăng Nhập";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
             this.Load += new System.EventHandler(this.frmLogin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.imageEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPass.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
-        private DevExpress.XtraEditors.SimpleButton btnXoa;
-        private DevExpress.XtraEditors.SimpleButton btnDangNhap;
-        private DevExpress.XtraEditors.SimpleButton btnThoat;
-        private DevExpress.XtraEditors.LabelControl labelControl1;
-        private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.PictureEdit imageEdit1;
-        private DevExpress.XtraEditors.TextEdit txtUsername;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
         private DevExpress.XtraEditors.ButtonEdit txtPass;
+        private DevExpress.XtraEditors.SimpleButton btnThoat;
+        private DevExpress.XtraEditors.SimpleButton btnDangNhap;
+        private DevExpress.XtraEditors.SimpleButton btnXoa;
+        private DevExpress.XtraEditors.TextEdit txtUsername;
     }
 }
